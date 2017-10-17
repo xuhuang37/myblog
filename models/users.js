5c2cb1a739dd1478086ae764a44f8ecf19a4ae67
huangxu <xuhuang37@outlook.com>
2017-10-18 01:08:14 +0800

    111

--- models/users.js
+++ models/users.js
@@ -1,6 +1,10 @@
-const User = require('../lib/mongo').User
+var User = require('../lib/mongo').User
 module.exports = {
     create:function(user) {
         return User.create(user).exec()
+    },
+    getUserByName:function getUserByName(name) {
+        return User.findOne({name: name}).addCreatedAt().exec()
     }
+
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+var PostModel = require('../models/posts')
 let checkLogin = require('../middlewares/check').checkLogin
 
 router.get('/', (req, res, next)=>{
@@ -9,11 +9,43 @@ router.get('/', (req, res, next)=>{
 })
 
 router.post('/', checkLogin, (req, res, next)=>{
-    res.send(req.flash())
+    var author = req.session.user._id;
+    var title = req.fields.title;
+    var content = req.fields.content;
+  
+    // 校验参数
+    try {
+      if (!title.length) {
+        throw new Error('请填写标题');
+      }
+      if (!content.length) {
+        throw new Error('请填写内容');
+      }
+    } catch (e) {
+      req.flash('error', e.message);
+      return res.redirect('back');
+    }
+  
+    var post = {
+      author: author,
+      title: title,
+      content: content,
+      pv: 0
+    };
+  
+    PostModel.create(post)
+      .then(function (result) {
+        // 此 post 是插入 mongodb 后的值，包含 _id
+        post = result.ops[0];
+        req.flash('success', '发表成功');
+        // 发表成功后跳转到该文章页
+        res.redirect(`/posts/${post._id}`);
+      })
+      .catch(next);
 })
 
 router.get('/create',checkLogin, (req, res, next)=>{
-    res.send(req.flash())
+    res.render('create')
 })
 
 router.get('/:postId',(req, res, next)=>{
